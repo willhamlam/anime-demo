855d262767de37cebedee1710b02bb4b18c8b659
will <will@willdeMac-mini.local>
2024-12-25 14:38:58 +0800

    add neon background effect:wq

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+#app {
+  width: 100%;
+  height: 100vh;
+}
+</style>
