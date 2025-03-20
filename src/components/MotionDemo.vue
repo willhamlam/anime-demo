316cc67dad9002ad5b8392fff66e467283a80571
Will <iamhandian@gmail.com>
2025-03-20 15:46:54 +0800

    add motiondemo

---
+++ src/components/MotionDemo.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { motion } from "motion-v";
+</script>
+
+<template>
+  <div class="container">
+    <motion.div
+      class="ball"
+      :initial="{ opacity: 0, scale: 0.5 }"
+      :animate="{ opacity: 1, scale: 1 }"
+      :transition="{
+        type: 'spring',
+        bounce: 0.6,
+      }"
+    />
+  </div>
+</template>
+
+<style>
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.ball {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background: #fad658;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-    <TheWelcome />
+  <MotionDemo />
 </template>
