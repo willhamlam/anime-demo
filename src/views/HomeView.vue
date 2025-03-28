dfd45479225cfe6517f3e847024479534517d0ed
will <will@willdeMac-mini.local>
2025-03-28 14:19:38 +0800

    add globe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import ShinyButton from '@/components/ShinyButton.vue';
+import GithubGlobe from '@/components/GithubGlobe.vue';
+// import ShinyButton from '@/components/ShinyButton.vue';
+
+const globeConfig = {
+  globeColor: '#fff', // 灰色
+  polygonColor: '#000', // 黑色
+};
 </script>
 
 <template>
-  <div class="flex h-80 items-center justify-center">
-    <ShinyButton>Shiny Button</ShinyButton>
+  <div class="flex h-screen items-center justify-center">
+    <GithubGlobe :globe-config="globeConfig" class="w-[1000px] h-[1000px]" />
   </div>
 </template>
