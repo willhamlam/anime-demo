cfb27af602d62317a3dc11613e2b41be199aba80
Will <iamhandian@gmail.com>
2025-03-29 16:20:11 +0800

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
-import Aurora from '../components/Aurora.vue'
+import Aurora from "../components/Aurora.vue";
+import GithubGlobe from "../components/GithubGlobe.vue";
 // import FontAnime from '../components/FontAnime.vue'
 // import MotionAnime from './MotionAnime.vue'
-import Background from '../components/Background.vue'
+import Background from "../components/Background.vue";
 </script>
 
 <template>
-    <Aurora />
+  <Aurora />
+  <GithubGlobe />
 </template>
